727ed0b5445eadd933a339f2099fd11070fe5b20
asrorbobomurodov <mr.aceonejr777@gmail.com>
2023-07-12 11:21:59 +0500

    done

--- basic_func05.py
+++ basic_func05.py
@@ -1,3 +1,5 @@
 # Create a function called main.
-
+def main():
+    return 5.6
 # Return the value float type.
+print(main())
--- basic_func06.py
+++ basic_func06.py
@@ -1,3 +1,5 @@
 # Create a function called main.
-
+def main():
+    return " Q: Who is the best player in the world?\n Ferdinad: Diego Maradona \n Q: Who is the best player in the world? \n CriRO: My name is Maradona \n 😂😂😂"
 # Return the value str type.
+print(main())
